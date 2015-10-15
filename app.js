43bcb6251543c0319f1b523ec6e4833dd959e8d3
brandly <brandly13@gmail.com>
2015-10-14 22:13:10 -0400

    make sure the handle is lowercased

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json())
 const handleMap = parseHandlesMap('./handles.txt')
 
 app.get(/[A-Za-z0-9_]{1,15}/i, function (req, res) {
-  const twitterHandle = req.path.slice(1)
+  const twitterHandle = req.path.slice(1).toLowerCase()
 
   if (twitterHandle in handleMap) {
     res.status(200).send({ snapchat: handleMap[twitterHandle] })
